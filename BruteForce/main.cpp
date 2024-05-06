521a2b3cb7b765f2334263cb710771e37d5c1009
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2024-05-06 23:03:31 +0300

    use brute force instead of binary search

--- BruteForce/main.cpp
+++ BruteForce/main.cpp
@@ -1,33 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <omp.h>
-#include <chrono>
-#include <algorithm>
 #include <utility>
+#include <chrono>
 
 using namespace std;
 using namespace std::chrono;
 
-vector<pair<int, int>> findAllPairsWithSumOMP(const vector<int>& arr, int targetSum, int threadsNum) {
+vector<pair<int, int>> findLocalPairsWithSum(const vector<int>& arr, int targetSum) {
     vector<pair<int, int>> pairs;
 
-    #pragma omp parallel num_threads(threadsNum)
-    {
-        vector<pair<int, int>> local_pairs;
-
-        #pragma omp for schedule(dynamic) nowait
-        for (size_t i = 0; i < arr.size(); ++i) {
-            for (size_t j = i + 1; j < arr.size(); ++j) {
-                if (arr[i] + arr[j] == targetSum) {
-                    local_pairs.emplace_back(arr[i], arr[j]);
-                }
+    for (int i = 0; i < arr.size(); ++i) {
+        for (int j = i + 1; j < arr.size(); ++j) {
+            if (arr[i] + arr[j] == targetSum) {
+                pairs.emplace_back(arr[i], arr[j]);
             }
         }
-
-        #pragma omp critical
-        pairs.insert(pairs.end(), local_pairs.begin(), local_pairs.end());
     }
-
     return pairs;
 }
 
@@ -40,11 +28,9 @@ vector<int> createLargeArray(size_t size) {
 }
 
 void printPairs(const vector<pair<int, int>>& pairs, int targetSum) {
-
     if (pairs.size() > 0) {
         cout << "The number of matching pairs for the required sum " << targetSum << ": " << pairs.size() << endl;
         cout << "The pairs: " << endl;
-
         for (const auto& p : pairs) {
             cout << "(" << p.first << ", " << p.second << ")" << endl;
         }
@@ -54,27 +40,22 @@ void printPairs(const vector<pair<int, int>>& pairs, int targetSum) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 4) {
-        cerr << "Please provide the array size and targetSum sum as command-line arguments." << endl;
+    if (argc < 3) {
+        cerr << "Please provide the array size, target sum, and number of threads as command-line arguments." << endl;
         return 1;
     }
-
     size_t arraySize = static_cast<size_t>(atoi(argv[1]));
-
     int targetSum = atoi(argv[2]);
 
-    int threadsNum = atoi(argv[3]);
-
     vector<int> arr = createLargeArray(arraySize);
-
     auto start = high_resolution_clock::now();
-    vector<pair<int, int>> result = findAllPairsWithSumOMP(arr, targetSum, threadsNum);
+    vector<pair<int, int>> pairs = findLocalPairsWithSum(arr, targetSum);
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
-    sort(result.begin(), result.end());
-    cout << "The number of threads: " << threadsNum << endl;
-    printPairs(result, targetSum);
-    cout << "Execution time: " << duration.count() << " milliseconds" << endl << endl;
+
+    cout << "===Sequential launch===" << endl;
+    printPairs(pairs, targetSum);
+    cout << "Execution time: " << duration.count() << " milliseconds" << endl;
 
     return 0;
 }
--- Lab2_MPI/main.cpp
+++ Lab2_MPI/main.cpp
@@ -1,62 +1,115 @@
-#include <mpi.h>
 #include <iostream>
 #include <vector>
+#include <utility>
 #include <chrono>
+#include <mpi.h>
 
-int binarySearch(const std::vector<int>& arr, int left, int right, int target) {
-    while (left <= right) {
-        if (target < left || target > right) {
-            return -1;
-        }
-        int mid = left + (right - left) / 2;
-        
-        if (arr[mid] == target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+using namespace std;
+using namespace std::chrono;
+
+vector<pair<int, int>> findLocalPairsWithSum(const vector<int>& arr, int targetSum, int startIdx, int endIdx) {
+    vector<pair<int, int>> local_pairs;
+
+    for (int i = startIdx; i < endIdx; ++i) {
+        for (int j = i + 1; j < arr.size(); ++j) {
+            if (arr[i] + arr[j] == targetSum) {
+                local_pairs.emplace_back(arr[i], arr[j]);
+            }
         }
     }
-    
-    return -1;
+    return local_pairs;
 }
 
-int main(int argc, char** argv) {
-    MPI_Init(&argc, &argv);
-    
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+vector<int> createLargeArray(size_t size) {
+    vector<int> arr;
+    for (int i = 1; i <= static_cast<int>(size) * 2; i += 2) {
+        arr.push_back(i);
+    }
+    return arr;
+}
 
-    std::vector<int> arr(10000);
-    for (int i = 0; i < arr.size(); i++) {
-        arr[i] = i;
+void printPairs(const vector<pair<int, int>>& pairs, int targetSum) {
+    if (pairs.size() > 0) {
+        cout << "The number of matching pairs for the required sum " << targetSum << ": " << pairs.size() << endl;
+        cout << "The pairs: " << endl;
+        for (const auto& p : pairs) {
+            cout << "(" << p.first << ", " << p.second << ")" << endl;
+        }
+    } else {
+        cout << "No pair found with sum " << targetSum << endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int world_size, world_rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    if (argc < 3) {
+        if (world_rank == 0) {
+            cerr << "Please provide the array size, target sum, and number of threads as command-line arguments." << endl;
+        }
+        MPI_Finalize();
+        return 1;
     }
 
-    int target = 9999;
-    int chunk_size = arr.size() / size;
-    int start = rank * chunk_size;
-    int end = (rank == size - 1) ? arr.size() - 1 : start + chunk_size - 1;
+    size_t arraySize = static_cast<size_t>(atoi(argv[1]));
+    int targetSum = atoi(argv[2]);
 
-    auto start_time = std::chrono::high_resolution_clock::now();
+    vector<int> arr;
+    if (world_rank == 0) {
+        arr = createLargeArray(arraySize);
+    }
 
-    int local_result = binarySearch(arr, start, end, target);
+    MPI_Bcast(&arraySize, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
 
-    int global_result = -1;
-    MPI_Reduce(&local_result, &global_result, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (world_rank != 0) {
+        arr.resize(arraySize);
+    }
 
-    if (rank == 0) {
-        auto end_time = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> execution_time = end_time - start_time;
-        
-        if (global_result != -1) {
-            std::cout << "Target value " << target << " found at index " << global_result << "\n";
-        } else {
-            std::cout << "Target value not found\n";
+    MPI_Bcast(arr.data(), arraySize, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int elements_per_process = arraySize / world_size;
+    int startIdx = world_rank * elements_per_process;
+    int endIdx = (world_rank == world_size - 1) ? arraySize : startIdx + elements_per_process;
+
+    auto start = high_resolution_clock::now();
+    vector<pair<int, int>> local_pairs = findLocalPairsWithSum(arr, targetSum, startIdx, endIdx);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+
+    vector<int> recv_counts(world_size);
+    vector<int> displs(world_size);
+
+    int local_size = local_pairs.size() * 2;
+    MPI_Gather(&local_size, 1, MPI_INT, recv_counts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (world_rank == 0) {
+        displs[0] = 0;
+        for (int i = 1; i < world_size; i++) {
+            displs[i] = displs[i - 1] + recv_counts[i - 1];
         }
-        
-        std::cout << "Parallel execution time: " << execution_time.count() * 1000 << " milliseconds\n";
+    }
+
+    vector<int> global_pairs;
+    if (world_rank == 0) {
+        global_pairs.resize(displs[world_size - 1] + recv_counts[world_size - 1]);
+    }
+
+    MPI_Gatherv(local_pairs.data(), local_size, MPI_INT, global_pairs.data(), recv_counts.data(), displs.data(), MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (world_rank == 0) {
+        vector<pair<int, int>> final_pairs;
+        for (size_t i = 0; i < global_pairs.size(); i += 2) {
+            final_pairs.emplace_back(global_pairs[i], global_pairs[i + 1]);
+        }
+
+        cout << "The number of processes: " << world_size << endl;
+        printPairs(final_pairs, targetSum);
+        cout << "Execution time: " << duration.count() << " milliseconds" << endl;
     }
 
     MPI_Finalize();
